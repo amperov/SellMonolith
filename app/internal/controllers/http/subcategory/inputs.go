30f62f75021cdffa7ff00c58c57d1a85ba6538b4
amperov <alex.amperov@yandex.ru>
2023-01-05 21:38:20 +0700

    created inputs

---
+++ app/internal/controllers/http/subcategory/inputs.go
@@ -0,0 +1,27 @@
+package subcategory
+
+type CreateSubcatInput struct {
+	Title string `json:"title,omitempty"`
+	CatID int    `json:"category_id"`
+}
+
+func (c *CreateSubcatInput) ToMap() map[string]interface{} {
+	var m map[string]interface{}
+
+	m["title"] = c.Title
+	m["category_id"] = c.CatID
+	return m
+}
+
+type UpdateSubcatInput struct {
+	Title string `json:"title,omitempty"`
+	CatID int    `json:"category_id"`
+}
+
+func (c *UpdateSubcatInput) ToMap() map[string]interface{} {
+	var m map[string]interface{}
+
+	m["title"] = c.Title
+	m["category_id"] = c.CatID
+	return m
+}
