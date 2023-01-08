4ca188cd0632c9df5a32987bb7d62d88d987e425
amperov <alex.amperov@yandex.ru>
2023-01-08 14:29:17 +0700

    added service

---
+++ app/internal/service/client/service.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"context"
+)
+
+type ClientDigi interface {
+	Auth(ctx context.Context, Username string) string
+	GetProducts(ctx context.Context, UniqueCode, Token string) ([]map[string]interface{}, error)
+}
+
+type ProductStore interface {
+	SearchByUniqueCode(ctx context.Context, UniqueCode string) (map[string]interface{}, bool)
+}
+
+type ClientService struct {
+	c ClientDigi
+	p ProductStore
+}
+
+func NewClientService(c ClientDigi, p ProductStore) *ClientService {
+	return &ClientService{c: c, p: p}
+}
+
+func (c *ClientService) Get(ctx context.Context, UniqueCode string, Username string) (map[string]interface{}, error) {
+	prods, ok := c.p.SearchByUniqueCode(ctx, UniqueCode)
+	if !ok {
+		var m map[string]interface{}
+		token := c.c.Auth(ctx, Username)
+		get, err := c.c.GetProducts(ctx, UniqueCode, token)
+
+		m["products"] = get
+		if err != nil {
+			return nil, err
+		}
+		return m, nil
+	}
+
+	return prods, nil
+}
