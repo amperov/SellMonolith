380552b3c652d1f519383ff4ddb943a1ecb9d333
amperov <alex.amperov@yandex.ru>
2023-01-08 14:30:28 +0700

    updated mw

--- app/pkg/auth/middleware.go
+++ app/pkg/auth/middleware.go
@@ -3,12 +3,17 @@ package auth
 import (
 	"context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strings"
 )
 
 type MiddleWare struct {
 	tm TokenManager
+}
+
+func NewMiddleWare(tm TokenManager) MiddleWare {
+	return MiddleWare{tm: tm}
 }
 
 func (w *MiddleWare) IsAuth(handle httprouter.Handle) httprouter.Handle {
@@ -19,14 +24,15 @@ func (w *MiddleWare) IsAuth(handle httprouter.Handle) httprouter.Handle {
 		if len(headerArray) != 2 {
 
 		}
-
+		logrus.Println(headerArray[1])
 		id, err := w.tm.ValidateToken(headerArray[1])
 		if err != nil {
+			logrus.Println(err)
 			return
 		}
-		ctx := context.WithValue(request.Context(), "user_id", id)
+		ctx := context.WithValue(request.Context(), "userID", id)
 		request.WithContext(ctx)
 
-		handle(writer, request, params)
+		handle(writer, request.WithContext(ctx), params)
 	}
 }
